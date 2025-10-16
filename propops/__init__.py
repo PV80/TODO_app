2b06ad31ae391a95469144d41dcbc36bf3e4300e
PV80 <130853691+PV80@users.noreply.github.com>
2025-10-16 15:05:34 +0000

    Remove all files

--- propops/__init__.py
+++
@@ -1,6 +0,0 @@
-"""PropOps AI (Kenya Edition) backend service."""
-
-from .database import initialize_database
-from .service import PropOpsService
-
-__all__ = ["initialize_database", "PropOpsService"]
